86c573da99522f26f7c64d8812977908415a771b
Vanisha1606 <vanishag1606@gmail.com>
2024-08-26 04:50:14 +0530

    Fixed some css styling errors

--- sign-up/src/Navbar.jsx
+++ sign-up/src/Navbar.jsx
@@ -11,25 +11,14 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <a href="/">NewsPortal</a>
-      </div>
-      <ul className="navbar-links">
-        <li><a href="/politics">Politics</a></li>
-        <li><a href="/sports">Sports</a></li>
-        <li><a href="/entertainment">Entertainment</a></li>
-        <li><a href="/tech">Tech</a></li>
-      </ul>
-      <div className="navbar-search">
-        <input type="text" placeholder="Search..." />
+        <a href="/">INSIGHT</a>
       </div>
       <div className="navbar-menu">
         <button className="menu-button" onClick={toggleMenu}>Menu</button>
-        {showMenu && (
-          <ul className="menu-dropdown">
-            <li><a href="/login">LogOut</a></li>
-            <li><a href="/ai-assistant-writing">AI Assistant Writing</a></li>
-          </ul>
-        )}
+        <ul className={`menu-dropdown ${showMenu ? 'show' : ''}`}>
+          <li><a href="/login">LogOut</a></li>
+          <li><a href="/ai-assistant-writing">AI Assistant Writing</a></li>
+        </ul>
       </div>
     </nav>
   );
